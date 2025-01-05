128cfe35d9db447296939d3ea344544eea694274
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-01-05 14:53:31 +0545

    refactor(presentation): update image handling and captions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ const Header = ({ children }: PropsWithChildren) => {
   return <h4 className={'text-8xl'}>{children}</h4>;
 };
 
-function PresentSection({ children, imageUrl }: React.PropsWithChildren<{ imageUrl: string }>) {
+function PresentSection({ children, imageUrl }: PropsWithChildren<{ imageUrl: string }>) {
   return (
     <section
       className={'bg-black min-h-screen bg-cover bg-center grid grid-cols-12 grid-rows-3'}
@@ -22,8 +22,9 @@ function PresentSection({ children, imageUrl }: React.PropsWithChildren<{ imageU
 
 const Poster = {
   hero: './luke-braswell-oYFv-_JKsVk-unsplash.jpg',
-  second: './thought-catalog--v7EOw5SA4I-unsplash.jpg',
-  third: './kimson-doan-HD8KlyWRYYM-unsplash.jpg',
+  second: './kimson-doan-HD8KlyWRYYM-unsplash.jpg',
+  third: './thought-catalog--v7EOw5SA4I-unsplash.jpg',
+  fourth: './juri-gianfrancesco-E05kCjekrwk-unsplash.jpg',
 };
 
 function Page() {
@@ -41,7 +42,7 @@ function Page() {
         </div>
       </PresentSection>
       {/*old https://source.unsplash.com/-v7EOw5SA4I*/}
-      <PresentSection imageUrl={Poster.third}>
+      <PresentSection imageUrl={Poster.second}>
         <div className={'row-start-2 col-start-3 text-white'}>
           <h3>Photography</h3>
           <Header>Creative</Header>
@@ -49,27 +50,35 @@ function Page() {
           <Header>Theme</Header>
         </div>
       </PresentSection>
-      <PresentSection imageUrl={Poster.second}>
+      <PresentSection imageUrl={Poster.third}>
         <div
           className={
             'masonary row-span-3 col-span-12 text-white grid grid-cols-3 grid-rows-2 p-20 gap-3 h-screen'
           }
         >
-          <figure className={'row-span-2'}>
-            <img alt='' src={Poster.second} />
-            <caption>Images Tell a Story</caption>
+          <figure className={'row-span-2 relative'}>
+            <img alt='' src={Poster.fourth} />
+            <caption className='absolute bottom-2 left-2 bg-black bg-opacity-50 text-white px-2 py-1 rounded'>
+              Thinking Images Tell a Story
+            </caption>
           </figure>
-          <figure className={'md:row-span-1 md:col-start-2'}>
+          <figure className={'md:row-span-1 md:col-start-2 relative'}>
             <img alt='' src={Poster.second} />
-            <caption>Images Tell a Story</caption>
+            <caption className='absolute bottom-2 left-2 bg-black bg-opacity-50 text-white px-2 py-1 rounded'>
+              Looking Images Tell a Story
+            </caption>
           </figure>
-          <figure className={'md:row-span-1 md:col-start-3'}>
-            <img alt='' src={Poster.second} />
-            <caption>Images Tell a Story</caption>
+          <figure className={'md:row-span-1 md:col-start-3 relative'}>
+            <img alt='' src={Poster.fourth} />
+            <caption className='absolute bottom-2 left-2 bg-black bg-opacity-50 text-white px-2 py-1 rounded'>
+              Being Images Tell a Story
+            </caption>
           </figure>
-          <figure className={'md:col-start-2 md:col-span-2 md:row-start-2 md:row-end-3'}>
+          <figure className={'md:col-start-2 md:col-span-2 md:row-start-2 md:row-end-3 relative'}>
             <img alt='' src={Poster.second} />
-            <caption>Images Tell a Story</caption>
+            <caption className='absolute bottom-2 left-2 bg-black bg-opacity-50 text-white px-2 py-1 rounded'>
+              All Images Tell a Story
+            </caption>
           </figure>
         </div>
       </PresentSection>
