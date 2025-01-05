a78bccd4e2b77363b142f66f00b3a54ab5cfa318
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-01-05 21:00:21 +0545

    feat: add ImageCard component and enhance TopSection layout

--- src/containers/home/top-section.tsx
+++ src/containers/home/top-section.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { ComponentPropsWithoutRef } from 'react';
+import type { ComponentPropsWithoutRef } from 'react';
 
 import { RotatedLogo } from '../../ui/rotated-logo.tsx';
 
@@ -10,7 +10,7 @@ function SmallParagraph({ children, className }: ComponentPropsWithoutRef<'p'>) 
 }
 function Top() {
   return (
-    <div className={'h-screen flex flex-col'} style={{ backgroundImage: `url(${imageUrl})` }}>
+    <div className={'min-h-screen flex flex-col'} style={{ backgroundImage: `url(${imageUrl})` }}>
       <div className={'flex flex-col flex-grow '}>
         <div className={'h-52'}></div>
         {/*  nav */}
@@ -28,7 +28,7 @@ function Top() {
                 </SmallParagraph>
               </div>
             </div>
-            <div className={'text-yellow'}>
+            <div className={'text-yellow text-8xl'}>
               UNIQUE <br />
               DESIGN ON STEP
             </div>
@@ -39,12 +39,31 @@ function Top() {
   );
 }
 
+function ImageCard({
+  children,
+  className,
+  src,
+}: ComponentPropsWithoutRef<'figcaption'> & { src: string }) {
+  return (
+    <figure className={clsx('relative', className)}>
+      <img
+        className={'aspect-[81/60] object-cover h-full w-full'}
+        alt='Thinking Images Tell a Story'
+        src={src}
+      />
+      <figcaption className='absolute bottom-0 left-0 bg-black  text-white p-4 rounded font-bold'>
+        {children}
+      </figcaption>
+    </figure>
+  );
+}
+
 export function TopSection() {
   return (
-    <section className={'text-white text-8xl bg-black'}>
+    <section className={'text-white  bg-black'}>
       <Top />
       <div className={'container mx-auto'}>
-        <div>
+        <div className={'text-8xl'}>
           WITH <br />
           THE TIMES
         </div>
@@ -55,6 +74,51 @@ export function TopSection() {
             business calls deliver a successful framework for both you and your audience.
           </SmallParagraph>
         </div>
+        <section className={'grid grid-cols-8 mt-40 gap-20'}>
+          <ImageCard className={'col-span-4'} src={'./laura-ockel-1kDBn0OSUGw-unsplash.jpg'}>
+            WE ASK RIGHT <br />
+            QUESTIONS
+          </ImageCard>
+
+          <SmallParagraph>
+            In the design process, they create both functional and beautiful things. The team
+            possesses unique individuality and strong qualifications and can easily translate
+            something so abstract and visionary into a digital experience. They always put the
+            clients first no matter how complicated the tasks are”.
+          </SmallParagraph>
+        </section>
+        <section className={'h-screen grid grid-cols-12 grid-rows-12 gap-10'}>
+          <div className='col-span-5 row-span-6 row-start-3'>
+            <ImageCard className='aspect-[1/1.3]' src={'./laura-ockel-1kDBn0OSUGw-unsplash.jpg'}>
+              {' '}
+              {/* This uses the default aspect ratio for video (16:9) */}
+              We achieve the goal
+            </ImageCard>
+          </div>
+          <div className='col-start-3 row-start-6 col-span-4 row-span-6'>
+            <ImageCard className='aspect-[1/1.2]' src={'./laura-ockel-1kDBn0OSUGw-unsplash.jpg'}>
+              {' '}
+              {/* Custom aspect ratio 81:53 */}
+              We develop a concept
+            </ImageCard>
+          </div>
+        </section>
+        <section className={'border-t-white border-t-2'}>
+          <article className={'flex py-20'}>
+            <div className={'text-6xl flex-grow'}>
+              OUR <br />
+              SERVICES
+            </div>
+            <div className={'flex-grow flex items-center'}>
+              <SmallParagraph className={'!text-xl'}>
+                We’ve worked with a wide array of clients across the globe to apply design
+                fundamentals of elegance, simplicity
+              </SmallParagraph>
+
+              <div>View all</div>
+            </div>
+          </article>
+        </section>
       </div>
     </section>
   );
