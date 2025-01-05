31c5632f27d8c2a9fb5ab9443fe9bb037f4f79a1
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-01-05 16:40:16 +0545

    feat: add unknown route page

---
+++ src/pages/[...all].tsx
@@ -0,0 +1,10 @@
+import Layout from "../layout";
+
+function Page() {
+    return (
+        <Layout>
+            <section className={'min-h-screen pt-52 container mx-auto text-white'}>Unknown</section>
+        </Layout>
+    )}
+
+export default Page;
